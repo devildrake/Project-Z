05c77e7929b60e04744c97211fb75bea69c497e4
Devildrake <davidsierragonzalez@enti.cat>
2016-11-17 11:16:17 +0100

    ESTE ES EL VERDADERO PARCHE 0.0.6

---
+++ ProjectZ/ProjectZ/Assets/AttackRangeScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackRangeScript : MonoBehaviour {
+
+    public int attackRadius;
+    public bool enemyInRange = false;
+
+    void Start()
+    {
+        GetComponent<CapsuleCollider>().radius = attackRadius;
+
+    }
+    // Update is called once per frame
+    void Update () {
+        
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Zn")
+        {
+            enemyInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Zn")
+        {
+            enemyInRange = false;
+        }
+    }
+}
---
+++ ProjectZ/ProjectZ/Assets/VillagerScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VillagerScript : MonoBehaviour {
+
+    VisionRangeScript laVision;
+    AttackRangeScript elAtaque;
+
+    public bool isAlive;
+    public int health;
+    public int attack;
+    public int defense;
+    public float attackSpeed;
+    public float attackCounter;
+
+    // Use this for initialization
+
+
+    void Start()
+    {
+
+
+        health = 100;
+        attack = 10;
+        defense = 10;
+        attackSpeed = 0.5f;
+        laVision = GetComponentInChildren<VisionRangeScript>();
+        elAtaque = GetComponentInChildren<AttackRangeScript>();
+    }
+
+    void Update ()
+    { 
+
+        if (laVision.enemyInSight) {
+            Debug.Log("Can see some");
+     
+
+        }
+        if (elAtaque.enemyInRange) {
+            Debug.Log("Should Attack");
+           // AttackEnemy();
+        }
+	}
+
+    void ApproachEnemy() {
+
+    }
+
+    void AttackEnemy(GameObject enemy)
+    {
+        attackCounter += Time.deltaTime;
+        if (attackCounter >= attackSpeed)
+        {
+            ZombieScript enemyScript = enemy.GetComponent<ZombieScript>();
+            enemyScript.health -= attack;
+            Debug.Log("I attacked");
+            attackCounter = 0;
+        }
+    }
+}
